c24e351ccb61c39956c2854509556245c6968227
kuji <kuji99@aol.com>
2022-12-05 00:18:13 +0300

    App.js

--- src/components/Qlist.js
+++ src/components/Qlist.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import { Row, Accordion, Button } from 'react-bootstrap'
+import { Data } from '../Data'
 
-const Qlist = ({lData}) => {
+const Qlist = ({lData, deleteOne}) => {
+  const jData= JSON.parse(localStorage.getItem("items"))
+
+  const onDleteitem= (ID)=>{
+    if(localStorage.getItem("items") != null){
+      const index = Data.findIndex((item)=> item.id === ID)
+      Data.splice(index,1)
+      deleteOne(Data)
+    }
+
+  }
   
 
   return (
     <Row>
         {
-          lData.length >=1 ? (lData.map((item,index)=>{
+          localStorage.getItem("items") != null ? (jData.map((item,index)=>{
         return (
           <Accordion className='my-2' key={index} defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
+        <Accordion.Item eventKey={item.id}>
         <Accordion.Header>{item.q}</Accordion.Header>
         <Accordion.Body >
         <div className='d-flex justify-content-between'>
         <div className='d-inline my-2 '>{item.a}</div>
-        <Button variant='info'>Delete</Button>
+        <Button onClick={()=> onDleteitem(item.id)} variant='info'>Delete</Button>
         </div>
         </Accordion.Body>
       </Accordion.Item>
